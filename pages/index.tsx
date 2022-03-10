876a53f627fac72a6b6917c4580fc6c2b6d0d66d
Volodymyr Smal-Stotskiy <volodyacm@gmail.com>
2022-03-11 00:25:22 +0200

    Add 'how it works' section

--- pages/index.tsx
+++ pages/index.tsx
@@ -36,6 +36,50 @@ const Home: NextPage = () => {
           />
         </div>
       </section>
+      <section className={`container container-mr ${styles['s-how-it-works']}`}>
+        <Htag asTitle className={styles.h1}>
+          How it works
+        </Htag>
+        <article className={styles['step']}>
+          <Image
+            src="/static/f7.png"
+            width={454}
+            height={364}
+            alt="Adapt your menu items"
+          />
+          <h1 className="text-center">Adapt your menu items</h1>
+          <p className="text-regular text-center">
+            Easily adapt your menu using the webflow CMS and allow customers to
+            browse your items.
+          </p>
+        </article>
+        <article className={styles['step']}>
+          <Image
+            src="/static/f8.png"
+            width={454}
+            height={364}
+            alt="Adapt your menu items"
+          />
+          <h1 className="text-center">Accept online orders & takeout</h1>
+          <p className="text-regular text-center">
+            Let your customers order and pay via the powerful ecommerce system,
+            or simple let them call your store.
+          </p>
+        </article>
+        <article className={styles['step']}>
+          <Image
+            src="/static/f9.png"
+            width={454}
+            height={364}
+            alt="Adapt your menu items"
+          />
+          <h1 className="text-center">Manage delivery or takeout</h1>
+          <p className="text-regular text-center">
+            Manage your own logistics and take orders simply through the
+            ecommerce system.
+          </p>
+        </article>
+      </section>
     </>
   );
 };
